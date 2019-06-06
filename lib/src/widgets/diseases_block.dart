ed54152e4a3a0cb52d94c0a1ad27593a9ee569e7
Sarayu <sarayugautam1@gmail.com>
2019-06-06 08:59:27 +0545

    diseases page completed

--- lib/src/widgets/diseases_block.dart
+++ lib/src/widgets/diseases_block.dart
@@ -12,61 +12,58 @@ class DiseaseBlock extends StatelessWidget {
       onTap: () {
         print('Here');
       },
-      child: Container(
-        // height: 150,
-        margin: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(5),
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 3,
-                spreadRadius: .03,
-                color: Colors.black.withOpacity(.3),
+      child: Material(
+        borderRadius: BorderRadius.circular(5),
+        elevation: 1.0,
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+          child: Row(
+            children: <Widget>[
+              Container(
+                child: Image.network(
+                  disease.imageUrl,
+                  height: 120,
+                  width: 140,
+                  fit: BoxFit.contain,
+                ),
               ),
-            ]),
-        child: Row(
-          children: <Widget>[
-            Container(
-              height: 140,
-              width: 100,
-              child: Image.network(
-                disease.imageUrl,
-                fit: BoxFit.contain,
+              SizedBox(
+                width: 15,
               ),
-            ),
-            SizedBox(
-              width: 15,
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    disease.name,
-                    overflow: TextOverflow.ellipsis,
-                    textAlign: TextAlign.start,
-                    maxLines: 3,
-                    style: Theme.of(context).textTheme.body1.copyWith(
-                          fontSize: 16,
-                          color: Colors.red[700],
-                        ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 10.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        disease.name,
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.start,
+                        maxLines: 3,
+                        style: Theme.of(context).textTheme.body1.copyWith(
+                              fontSize: 16,
+                              color: Colors.red[700],
+                            ),
+                      ),
+                      SizedBox(
+                        height: 13,
+                      ),
+                      Text(disease.body,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.justify,
+                          maxLines: 5,
+                          style: Theme.of(context)
+                              .textTheme
+                              .body2
+                              .copyWith(fontSize: 14)),
+                    ],
                   ),
-                  SizedBox(
-                    height: 13,
-                  ),
-                  Text(
-                    disease.body,
-                    overflow: TextOverflow.ellipsis,
-                    textAlign: TextAlign.start,
-                    maxLines: 5,
-                    style: Theme.of(context).textTheme.body2,
-                  ),
-                ],
-              ),
-            )
-          ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/src/widgets/search_box.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class SearchBox extends StatelessWidget {
+  const SearchBox({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 10, top: 20, right: 17, bottom: 20),
+      child: Material(
+        elevation: 3,
+        borderRadius: BorderRadius.all(
+          Radius.circular(30),
+        ),
+        child: TextField(
+          style: Theme.of(context).textTheme.body1.copyWith(
+                fontSize: 20,
+              ),
+          cursorColor: Colors.red[700],
+          decoration: InputDecoration(
+              focusedBorder: InputBorder.none,
+              border: InputBorder.none,
+              contentPadding:
+                  EdgeInsets.symmetric(horizontal: 20.0, vertical: 14.0),
+              suffixIcon: GestureDetector(
+                onTap: () {},
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                      color: Theme.of(context).scaffoldBackgroundColor,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Theme.of(context).textTheme.body1.color,
+                            spreadRadius: 0.03,
+                            blurRadius: 2)
+                      ]),
+                  child: Icon(
+                    Icons.search,
+                    color: Colors.red[700],
+                  ),
+                ),
+              )),
+        ),
+      ),
+    );
+  }
+}
