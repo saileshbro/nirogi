c2aaa59950bd6d28007a96a4cd0379ce546c9fd6
Sarayu <sarayugautam1@gmail.com>
2019-06-25 14:52:25 +0545

    Modified for making popup menu

--- lib/src/models/popupchoice.dart
+++ lib/src/models/popupchoice.dart
@@ -11,3 +11,20 @@ const List<Choice> choice = const <Choice>[
     icon: 'assets/images/icons/password.png',
   ),
 ];
+
+class ForumChoice {
+  const ForumChoice({this.title, this.icon});
+  final String title;
+  final String icon;
+}
+
+const List<ForumChoice> forumChoice = const <ForumChoice>[
+  const ForumChoice(
+    title: 'Most Popular',
+    icon: 'assets/images/icons/popular.png',
+  ),
+  const ForumChoice(
+    title: 'Most Recent',
+    icon: 'assets/images/icons/recent.png',
+  ),
+];
--- lib/src/widgets/choice_card.dart
+++ lib/src/widgets/choice_card.dart
@@ -12,3 +12,26 @@ class ChoiceCard extends StatelessWidget {
         style: Theme.of(context).textTheme.body1.copyWith(fontSize: 14));
   }
 }
+
+class ForumChoiceCard extends StatelessWidget {
+  const ForumChoiceCard({Key key, this.choice}) : super(key: key);
+
+  final ForumChoice choice;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Text(choice.title,
+            style: Theme.of(context).textTheme.body1.copyWith(fontSize: 14)),
+        SizedBox(
+          width: 15,
+        ),
+        Image.asset(
+          choice.icon,
+          width: 25,
+        )
+      ],
+    );
+  }
+}
